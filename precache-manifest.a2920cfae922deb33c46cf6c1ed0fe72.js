6d7ce946028325c4f81be58e6d88b3402e64e662
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-10 15:54:59 +0530

    Updates

---
+++ precache-manifest.a2920cfae922deb33c46cf6c1ed0fe72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44f55c46f2b53f18ee8a3656161537d",
+    "url": "/hangman-react/index.html"
+  },
+  {
+    "revision": "260695dc96a518d035c6",
+    "url": "/hangman-react/static/css/main.0998170a.chunk.css"
+  },
+  {
+    "revision": "f3293ff065b04177a034",
+    "url": "/hangman-react/static/js/2.0e429a6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-react/static/js/2.0e429a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260695dc96a518d035c6",
+    "url": "/hangman-react/static/js/main.7126a10b.chunk.js"
+  },
+  {
+    "revision": "3f7c69ac9d5e568f51ba",
+    "url": "/hangman-react/static/js/runtime-main.0e2bf36f.js"
+  }
+]);
